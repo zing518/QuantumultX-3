4dd2b4f5a61c94ee8b329cf6809dc6c6b99c7534
ziyong <61528323+Kyle0816@users.noreply.github.com>
2021-04-13 21:02:07 +0800

    Update jd_hby.js

--- JS/jd_hby.js
+++ JS/jd_hby.js
@@ -1,40 +1,32 @@
 /*
 只能用一天。需每天寻找直播抓包
-1 8-23 13 4 * https://raw.githubusercontent.com/Kyle0816/QuantumultX/main/JS/jd_hby.js, tag=hongbaoyu, enabled=true
+1 8-23 * * * lxk0301_live_redrain.js
 */
 const $ = new Env('整点京豆雨');
 let allMessage = '';
 let bodyList = {
-    "13": {
-    "url": "https://api.m.jd.com/client.action?functionId=liveActivityV946&uuid=8888888&client=apple&clientVersion=9.4.1&st=1617851035011&sign=ce3542c09121b1474c0beba1413fec13&sv=112",
-    "body": "body=%7B%22liveId%22%3A%223866703%22%7D"
+  '20': {
+    url: 'https://api.m.jd.com/client.action?functionId=liveActivityV842&uuid=8888888&client=apple&clientVersion=9.4.4&st=1616204859304&sign=a52a5ba5b42a43ce8d81e0014ba04859&sv=121',
+    body: 'body=%7B%22liveId%22%3A%223689733%22%7D'
   }
 }
 let ids = {
-  '0': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '1': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '2': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '3': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '4': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '5': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '6': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '7': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '8': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '9': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '10': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '11': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '12': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '13': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '14': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '15': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '16': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '17': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '18': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '19': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '20': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '21': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '22': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN', 
-  '23': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN'
+  '8': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '9': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '10': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '11': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '12': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '13': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '14': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '15': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '16': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '17': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '18': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '19': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '20': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '21': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '22': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '23': 'RRA4RhWMc159kA62qLbaEa88evE7owb'
 }
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
