8257c50d3d76c0e569c6c33635448e381ae4b94b
ziyong <61528323+Kyle0816@users.noreply.github.com>
2021-04-24 00:20:45 +0800

    Update jd_hby.js

--- JS/jd_hby.js
+++ JS/jd_hby.js
@@ -11,18 +11,30 @@ let bodyList = {
   }
 }
 let ids = {
-  '12': 'RRAzn5yQXcYJFMzyucMtGntDEgQUcV',
-  '13': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN',
-  '14': 'RRA85AvVYJBvkmwpDhn4Y9cMRXV9mH',
-  '15': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN',
-  '16': 'RRA2u4bEZ2zLYr9PKfwqfXngbVCmqNE',
-  '17': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN',
-  '18': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD',
-  '19': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN',
-  '20': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN',
-  '21': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN',
-  '22': 'RRA3gsDuBBP5kYqknjTZ2h3VLkEpiT3',
-  '23': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN'
+  '0': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '1': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '2': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '3': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '4': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '5': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '6': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '7': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '8': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '9': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '10': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '11': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '12': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '13': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '14': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '15': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '16': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '17': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '18': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '19': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '20': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '21': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '22': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '23': 'RRA4RhWMc159kA62qLbaEa88evE7owb'
 }
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
